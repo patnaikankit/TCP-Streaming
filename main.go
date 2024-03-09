786fc60c5864405d0d255bef6e8590ffa1be2f6d
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2024-03-09 18:57:38 +0530

    final

--- main.go
+++ main.go
@@ -1,82 +1,136 @@
 package main
 
 import (
-	"bytes"
 	"crypto/rand"
 	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 )
 
 type FileServer struct {
+	shutdownSignal chan os.Signal
+	waitGroup      sync.WaitGroup
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Println("Error ->", err)
+	}
+}
+
+func (fs *FileServer) handleSignals() {
+	signal.Notify(fs.shutdownSignal, syscall.SIGINT, syscall.SIGTERM)
+
+	// Wait for a signal to gracefully shutdown
+	<-fs.shutdownSignal
+
+	// Notify goroutines to finish
+	fs.waitGroup.Done()
 }
 
 func start(fs *FileServer) {
 	event, err := net.Listen("tcp", ":3000")
+	handleError(err)
 
-	if err != nil {
-		log.Fatal("Error -> ", err)
-	}
+	go fs.handleSignals()
 
 	for {
 		conn, err := event.Accept()
-
 		if err != nil {
-			log.Fatal("Error -> ", err)
+			select {
+			case <-fs.shutdownSignal:
+				return
+			default:
+				log.Println("Error accepting connection ->", err)
+			}
 		}
 
+		fs.waitGroup.Add(1)
 		go fs.readLoop(conn)
 	}
 }
 
 func (fs *FileServer) readLoop(conn net.Conn) {
-	buffer := new(bytes.Buffer)
-	for {
-		var size int64
-		binary.Read(conn, binary.LittleEndian, &size)
-		n, err := io.CopyN(buffer, conn, size)
+	defer fs.waitGroup.Done()
 
-		if err != nil {
-			log.Fatal("Error -> ", err)
+	var size int64
+	err := binary.Read(conn, binary.LittleEndian, &size)
+	if err != nil {
+		if err != io.EOF {
+			log.Println("Error reading size ->", err)
 		}
-
-		fmt.Println(buffer.Bytes())
-		fmt.Printf("%d bytes of data received over the network\n", n)
+		return
 	}
+
+	if size <= 0 {
+		log.Println("Invalid file size received:", size)
+		return
+	}
+
+	// Read file data directly from the connection
+	fileData := make([]byte, size)
+	n, err := io.ReadFull(conn, fileData)
+	if err != nil {
+		log.Println("Error reading data ->", err)
+		return
+	}
+
+	fmt.Println("----------------------------------------")
+	fmt.Printf("%d bytes of data received over the network\n", n)
+	fmt.Println(fileData)
 }
 
 func sendFile(size int) error {
+	if size <= 0 {
+		log.Println("Invalid file size:", size)
+		return fmt.Errorf("invalid file size")
+	}
+
 	file := make([]byte, size)
 	_, err := io.ReadFull(rand.Reader, file)
-
 	if err != nil {
-		log.Fatal("Error -> ", err)
+		log.Println("Error generating random data ->", err)
+		return err
 	}
 
 	conn, err := net.Dial("tcp", "localhost:3000")
 	if err != nil {
-		log.Fatal("Error -> ", err)
+		log.Println("Error connecting to server ->", err)
+		return err
 	}
+	defer conn.Close()
 
-	binary.Write(conn, binary.LittleEndian, int64(size))
+	handleError(binary.Write(conn, binary.LittleEndian, int64(size)))
 
-	n, err := io.CopyN(conn, bytes.NewReader(file), int64(size))
+	n, err := conn.Write(file)
 	if err != nil {
-		log.Fatal("Error -> ", err)
+		log.Println("Error writing data ->", err)
+		return err
 	}
+
 	fmt.Printf("%d bytes of data written over the network\n", n)
 	return nil
 }
 
 func main() {
+	fileServer := &FileServer{
+		shutdownSignal: make(chan os.Signal, 1),
+	}
+
 	go func() {
 		time.Sleep(4 * time.Second)
-		sendFile(4000)
+		handleError(sendFile(4000))
 	}()
 
-	server := &FileServer{}
-	start(server)
+	start(fileServer)
+
+	// Wait for all goroutines to finish before exiting
+	fileServer.waitGroup.Wait()
 }
